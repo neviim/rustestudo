2392497218572ed9466b09274d75d83814055cd4
Neviim <neviimdev@gmail.com>
2020-09-22 16:59:42 -0300

    usando json

--- json_serde/src/main.rs
+++ json_serde/src/main.rs
@@ -2,6 +2,7 @@ mod rotinas;
 
 fn main() {
 
-    rotinas::jsons::phone();
+    rotinas::jsons::phone_modelo1();
+    rotinas::jsons::phone_modelo2();
 
 }
--- json_serde/src/rotinas/jsons.rs
+++ json_serde/src/rotinas/jsons.rs
@@ -1,6 +1,6 @@
 use serde_json::json;
 
-pub fn phone() {
+pub fn phone_modelo1() {
   // O tipo de `john` é `serde_json::Value`
   let john = json!({
     "name": "Neviim Jads",
@@ -16,4 +16,28 @@ pub fn phone() {
 
   // Converter para string o JSON e mostrar na saida
   println!("{}", john.to_string());
+}
+
+fn random_phone() -> String {
+  // Not too random.
+  "1234567".to_owned()
+}
+
+pub fn phone_modelo2() {
+
+  let nome_completo = "Neviim Jads";
+  let idade_ultimo_ano = 42;
+  
+  // O tipo de `john` é `serde_json::Value`
+  let john = json!({
+      "name": nome_completo,
+      "age": idade_ultimo_ano + 1,
+      "phones": [
+          format!("+55 {}", random_phone())
+      ]
+  });
+
+  println!();
+  println!("{}", john.to_string());
+
 }
